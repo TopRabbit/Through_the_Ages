c3048dc9c4685cadca1e736e4205a4bd7bb6e506
TopRabbit <bdfnoff@yandex.ru>
2022-10-22 13:12:01 +0300

    начал создавать игру. файл main для управления игрой, cards для описания карточек, players для описания состояния игроков

---
+++ cards.py
@@ -0,0 +1,4 @@
+class card:
+    def __int__(self):
+        age=a,1,2,3
+        type=peace, war
---
+++ players.py
@@ -0,0 +1,17 @@
+class player:
+    def __int__(self):
+        color=
+        Warriors
+        farm
+        mine
+        Philosophy
+        Religion
+        Despotism
+
+        blue_bank=[5,5,6]
+        marker_of_science=1
+        marker_of_culture = 0
+        marker_of_strength=0
+        marker_of_happiness=0
+
+        Tactics_Standard
